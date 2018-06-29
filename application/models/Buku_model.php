e0bb1e1e3bc7313194b5784daee4905c608e03fb
serlynovita <38042755+serlynovita@users.noreply.github.com>
2018-06-29 16:09:21 +0700

    buku model

---
+++ application/models/Buku_model.php
@@ -0,0 +1,52 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Buku_model extends CI_Model {
+
+	public function __construct()
+	{
+		parent::__construct();
+		
+	}
+	public function insertBuku()
+	{
+			$object=array(
+			'judul'=>$this->input->post('judul'),
+			'pengarang'=>$this->input->post('pengarang'),
+			'penerbit'=>$this->input->post('penerbit'),
+			'tahun_terbit'=>$this->input->post('tahun_terbit'),
+			'jumlah_halaman'=>$this->input->post('jumlah_halaman'),
+			'gambar'=>$this->upload->data('file_name'),
+			'sinopsis'=>$this->input->post('sinopsis'),
+			'stok'=>$this->input->post('stok'),
+			'harga'=>$this->input->post('harga'));
+
+		$this->db->insert('buku', $object);
+	}
+	public function getBuku_list()
+	{
+		$query = $this->db->query("Select buku.*, kategori.* from buku,kategori where buku.id_kategori = kategori.id_kategori");
+		return $query->result_array();
+	}
+	public function updateById($id)
+	{
+		$data = array('judul' => $this->input->post('judul'),
+			'pengarang'=>$this->input->post('pengarang'),  
+			'penerbit'=>$this->input->post('penerbit'), 
+			'tahun_terbit'=>$this->input->post('tahun_terbit'),
+			'jumlah_halaman'=>$this->input->post('jumlah_halaman'),
+			'sinopsis'=>$this->input->post('sinopsis'),
+			'stok'=>$this->input->post('stok'),
+			'harga'=>$this->input->post('harga'),);
+		$this->db->where('id_buku', $id);
+		$this->db->update('buku', $data);
+	}
+	
+	public function delete($id){
+		$this->db->where('id_buku', $id);
+		$this->db->delete('buku');
+	}
+}
+
+/* End of file buku_model.php */
+/* Location: ./application/models/buku_model.php */
