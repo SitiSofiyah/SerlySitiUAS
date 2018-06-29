5feed020a83adef640ef2170ab277bb64619bd0f
serlynovita <38042755+serlynovita@users.noreply.github.com>
2018-06-29 18:24:50 +0700

    perbaikan controller buku

--- application/controllers/Buku.php
+++ application/controllers/Buku.php
@@ -2,12 +2,36 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Buku extends CI_Controller {
-
+	public function __construct()
+	{
+		parent::__construct();
+		if($this->session->userdata('logged_in')){
+			$session_data=$this->session->userdata("logged_in");
+			$data['username']=$session_data['username'];
+			$data['level']=$session_data['level'];
+			$current_controller = $this->router->fetch_class();
+			$this->load->library('acl');
+			if(! $this->acl->is_public($current_controller)){
+				if(! $this->acl->is_allowed($current_controller, $data['level'])){
+					redirect('login/logout','refresh');
+				}
+			}
+		}else{
+			redirect('login','refresh');
+		}
+	}
 	public function index()
 	{
 		$this->load->model('buku_model');
 		$object["buku_list"] = $this->buku_model->getBuku_list();
-		$this->load->view('buku_list', $object);
+		$this->load->view('dashboardAdmin', $object);
+	}
+
+	public function databuku()
+	{
+		$this->load->model('buku_model');
+		$object["buku_list"] = $this->buku_model->getBuku_list();
+		$this->load->view('buku_view', $object);
 	}
 
 	
